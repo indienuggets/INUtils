ba45d0793836cfcbc428fa9c99073888921a9809
Indie Nuggets <36259318+indienuggets@users.noreply.github.com>
2018-05-10 22:19:29 +0900

    Add files via upload

---
+++ Color Cycler/Editor/ColorCyclerEditor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(ColorCycler))]
+class ColorCyclerEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+
+        if (GUILayout.Button("Cycle"))
+        {
+            ColorCycler cycler = (ColorCycler)target;
+            cycler.Cycle();
+        }
+    }
+}
---
+++ Timer.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class Timer : MonoBehaviour
+{
+
+    public float RunEvery = 5;
+    public bool RunOnAwake = false;
+    public bool Loop = true;
+    public UnityEvent OnTimerEnd;
+
+    void OnEnable()
+    {
+        if (RunOnAwake)
+        {
+            Invoke("Execute", 0);
+        }
+
+        if (Loop)
+        {
+            InvokeRepeating("Execute", RunEvery, RunEvery);
+        }
+        else
+        {
+            Invoke("Execute", RunEvery);
+        }
+    }
+
+    public void Execute()
+    {
+        OnTimerEnd.Invoke();
+    }
+}
